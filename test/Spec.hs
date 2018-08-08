2c9cee0aa2ba2d6ef21c09c24fb4ddb93b78e960
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-08-08 16:08:08 +0900

    better test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,32 +1,16 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings, OverloadedLabels, DataKinds, TypeOperators, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
 import Control.Monad.Fix
-import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 import Data.Winery
-import Data.Winery.Term
-import GHC.Generics
+import qualified Data.Winery.Internal.Builder as WB
+import Test.QuickCheck
+import Control.Monad
 
-data TestRec = TestRec
-    { foo :: Maybe Int
-    , bar :: [ByteString]
-    , nodes :: [TestRec]
-    } deriving (Show, Generic)
+prop_VarInt :: [Int] -> Property
+prop_VarInt i = B.length bs === WB.getSize e .&&. decodeCurrent bs === i
+  where
+    bs = WB.toByteString e
+    e = toEncoding i
 
-instance Serialise TestRec where
-  schemaVia = gschemaViaRecord
-  toEncoding = gtoEncodingRecord
-  deserialiser = TestRec
-    <$> extractField "foo"
-    <*> extractField "bar"
-    <*> extractFieldWith (extractListWith deserialiser) "nodes"
-
-defTest :: TestRec
-defTest = TestRec Nothing ["hello"] [TestRec (Just 42) ["world"] []]
-
-main :: IO ()
-main = return ()
-
-data TestVar = VFoo | VBar !Int | VBaz !Bool !Bool deriving (Show, Generic)
-
-instance Serialise TestVar
-
-newtype UserId = UserId Int deriving Serialise
+return []
+main = void $ $quickCheckAll
