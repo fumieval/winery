993ff3826288fec9098868185e7a1dda8afd632b
Fumiaki Kinoshita <fumiexcel@gmail.com>
2019-01-25 15:55:46 +0900

    fix the test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
-import Control.Monad.Fix
-import qualified Data.ByteString as B
+import qualified Data.ByteString.FastBuilder as B
 import Data.Winery
-import qualified Data.Winery.Internal.Builder as WB
+import Data.Winery.Internal
 import Test.QuickCheck
 import Control.Monad
 
-prop_VarInt :: [Int] -> Property
-prop_VarInt i = B.length bs === WB.getSize e .&&. decodeCurrent bs === i
-  where
-    bs = WB.toByteString e
-    e = toEncoding i
+prop_VarInt :: Int -> Property
+prop_VarInt i = evalDecoder decodeVarInt
+  (B.toStrictByteString $ varInt i) === i
 
 return []
 main = void $ $quickCheckAll
