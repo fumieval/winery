8e3e56cbd02e220cf7070df21ea36f432f82711e
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-10-11 01:44:15 +0900

    megaparsec >= 7.0.0

--- app/Main.hs
+++ app/Main.hs
@@ -86,11 +86,7 @@ main = getOpt Permute options <$> getArgs >>= \case
     let o = foldl (flip id) defaultOptions fs
     q <- case parse (parseQuery <* eof) "argument" $ T.pack qs of
       Left e -> do
-#if MIN_VERSION_megaparsec(7,0,0)
         hPutStrLn stderr $ errorBundlePretty e
-#else
-        hPutStrLn stderr $ parseErrorPretty e
-#endif
         exitWith (ExitFailure 2)
       Right a -> pure a
     forM_ paths $ \case
